3dd6cfca3a980cd76f3f7ac0173112d7f01955a1
NJKatorz <nathanesakwa@gmail.com>
2023-10-25 21:42:58 +0200

    module 7: 7.1-7.3

---
+++ exercices/module7/7.1-7.3/src/components/App/App.jsx
@@ -0,0 +1,78 @@
+import { useState, useEffect } from 'react'
+import {
+  BrowserRouter as Router,
+  Routes, Route, Link
+} from 'react-router-dom'
+
+import Menu from 'components/Menu/Menu'
+import CreateNew from 'components/CreateNew/CreateNew'
+import AnecdoteList from 'components/Anecdotes/AnecdoteList'
+import About from 'components/About/About'
+import Footer from 'components/Footer/Footer'
+import OneAnecdote from 'components/Anecdotes/OneAnecdote'
+import Notification from 'components/Notification/Notification'
+
+const App = () => {
+  const [anecdotes, setAnecdotes] = useState([
+    {
+      content: 'If it hurts, do it more often',
+      author: 'Jez Humble',
+      info: 'https://martinfowler.com/bliki/FrequencyReducesDifficulty.html',
+      votes: 0,
+      id: 1
+    },
+    {
+      content: 'Premature optimization is the root of all evil',
+      author: 'Donald Knuth',
+      info: 'http://wiki.c2.com/?PrematureOptimization',
+      votes: 0,
+      id: 2
+    }
+  ])
+
+  const [notification, setNotification] = useState('')
+
+  const addNew = (anecdote) => {
+    anecdote.id = Math.round(Math.random() * 10000)
+    setAnecdotes(anecdotes.concat(anecdote))
+    setNotification(`a new anecdote ${anecdote.content} created!`)
+  }
+
+  const clearNotificationWithDelay = () => {
+    setTimeout(() => setNotification(''), 5000);
+  };
+
+  useEffect(clearNotificationWithDelay, [notification]);
+
+
+  const anecdoteById = (id) =>
+    anecdotes.find(a => a.id === id)
+
+  const vote = (id) => {
+    const anecdote = anecdoteById(id)
+
+    const voted = {
+      ...anecdote,
+      votes: anecdote.votes + 1
+    }
+
+    setAnecdotes(anecdotes.map(a => a.id === id ? voted : a))
+  }
+
+  return (
+    <div>
+      <h1>Software anecdotes</h1>
+      <Menu />
+      {notification ? <Notification notification={notification} /> : <div></div> }
+      <Routes>
+        <Route path="/" element={<AnecdoteList anecdotes={anecdotes} />} />
+        <Route path="/create" element={<CreateNew addNew={addNew} />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/anecdotes/:id" element={<OneAnecdote anecdotes={anecdotes} />} />
+      </Routes>
+      <Footer />
+    </div>
+  )
+}
+
+export default App;
